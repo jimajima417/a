dbadaaa22e2c118858d2a574fe69bac2fb6f2880
s21b718 <s21b718@shohoku.ac.jp>
2021-12-17 10:11:52 +0900

    MainActivitykからGamen2、Gamen3への画面遷移をしました。

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -22,5 +22,23 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button changeButton2 = findViewById(R.id.button2);
+        changeButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), Gamen2.class);
+                startActivity(intent);
+            }
+        });
+
+        Button changeButton3 = findViewById(R.id.button3);
+        changeButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), Gamen3.class);
+                startActivity(intent);
+            }
+        });
     }
 }
